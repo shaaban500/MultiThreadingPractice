ba16cbc7303d6581b61eb7d509a6e7e3e7bec4b1
shaaban500 <moshaaban500@gmail.com>
2022-09-07 14:55:56 +0200

    Add project files.

---
+++ MultiThreadingPractice/Program.cs
@@ -0,0 +1,28 @@
+﻿Thread x = new Thread(WriteY); 
+x.Start();
+
+Thread y = new Thread(WriteX);
+y.Start();
+
+Thread z = new Thread(WriteZ);
+z.Start();
+
+for (int i = 0; i < 1000; i++)
+{
+    Console.Write("a");
+}
+
+static void WriteY()
+{
+    for (int i = 0; i < 1000; i++) Console.Write("b");
+}
+
+static void WriteX()
+{
+    for (int i = 0; i < 1000; i++) Console.Write("c");
+}
+
+static void WriteZ()
+{
+    for (int i = 0; i < 1000; i++) Console.Write("d");
+}
